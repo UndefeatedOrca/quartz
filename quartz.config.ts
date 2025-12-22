4c38cacca48793ca7a4d3c44abcd06a781df7d54
UndefeatedOrca <122404944+UndefeatedOrca@users.noreply.github.com>
2025-12-22 16:36:20 -0500

    Quartz sync: Dec 22, 2025, 4:36 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "PaJoFo",
+    pageTitle: "PaJoFo Ponderings",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "undefeatedorca.github.io",
+    baseUrl: "pajofo.net",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
